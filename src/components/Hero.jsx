fc61c7d91ec7b188d96e2f67c76dcb88a18ad088
layuntaFelipe <layunta.felipe@gmail.com>
2022-08-09 16:32:25 -0300

    hero-text-change

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
       <img src={heroIMG} className="heroImage" alt="" />
       <div className="heroInfo">
         <h1>Hi! I'm <span className='letters'>F</span><span className='letters'>e</span><span className='letters'>l</span><span className='letters'>i</span><span className='letters'>p</span><span className='letters'>e</span>, Junior Full Stack Developer</h1>
-        <p>A passionate Full Stack Developer with more than 3 years of programming experience (Mobile Development + Web Development). As a Full Stack Web Developer, I bring to the table experience of building a real-world application using ReactJS, Javascript, NodeJS, Express, Firebase, MongoDB, and other cool and fun libraries that makes our lives better and easier.</p>
+        <p>A passionate Full Stack Developer with more than 3 years of programming experience (Mobile Development + Web Development). As a Full Stack Web Developer, I bring to the table experience of building a real-world application using ReactJS, Javascript, NodeJS, Express, Firebase, MongoDB, and other cool and fun libraries that makes me ready to help in any way possible with the technologies I already know and with those that I don't know yet.</p>
         <Link to='contact' spy={true} smooth={true} offset={-100} duration={500}><span className='goToContact'>Lets Talk</span></Link>
       </div>
     </div>
