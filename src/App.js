cc34c16a5f8597f4ce7f95ee00178d3e011382a5
layuntaFelipe <layunta.felipe@gmail.com>
2022-08-09 10:18:45 -0300

    background-setting-3

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   const [iconsNumber, setIconsNumber] = useState([]);
 
   useEffect(() => {
-    const newState = buildRandomIcons((document.documentElement.clientWidth + 300));
+    const newState = buildRandomIcons((document.documentElement.clientWidth * 1.5));
     setIconsNumber((prevState) => [...prevState, newState]);
     console.log(document.documentElement.clientWidth);
   }, []);
