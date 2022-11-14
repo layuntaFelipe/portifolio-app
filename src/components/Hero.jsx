f5583bdbcab86c4d5a72d932b6291002feae5f22
layuntaFelipe <layunta.felipe@gmail.com>
2022-11-13 23:50:55 -0700

    testing-new

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,6 +12,7 @@ const Hero = () => {
         <p>A passionate Full Stack Developer with 2+ years of programming experience (Mobile Development + Web Development). As a Full Stack Web Developer, I bring to the table experience of building a real-world application using ReactJS, Javascript, NodeJS, Express, Firebase, MongoDB, and other cool and fun libraries that makes me ready to help in any way possible with the technologies I already know and with those that I don't know yet.</p>
         <Link to='contact' spy={true} smooth={true} offset={-100} duration={500}><span className='goToContact'>Lets Talk</span></Link>
         <a href={pdfCV} className='downloadCV' download="Felipe_Web_Dev_CV">Download CV</a>
+        <p>Certifications</p>
       </div>
     </div>
   )
